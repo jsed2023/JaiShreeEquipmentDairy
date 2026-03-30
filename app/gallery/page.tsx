d7c4e851feea023dd1c7c2080a84454606b0be3b
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-03-30 22:14:12 +0530

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -90,17 +90,16 @@ export default function GalleryPage() {
       {/* ✅ Preload LCP image */}
       <link rel="preload" as="image" href={getCldUrl(images[0].src, 800)} />
 
-            <div className="items-center justify-center gap-2 px-3 py-2.5 
-        rounded-full text-base font-semibold
-        bg-gradient-to-r from-purple-500/10 via-blue-500/10 to-cyan-500/10
-        text-purple-700 dark:text-purple-300
-        border border-purple-400/30 backdrop-blur-sm">
+            <div className="flex w-fit mx-auto items-center justify-center px-4 py-1.5 mb-4 
+    rounded-full font-semibold
+    bg-gradient-to-r from-purple-500/10 via-blue-500/10 to-cyan-500/10
+    border border-purple-400/30 backdrop-blur-sm">
 
-        <h1 className="text-2xl md:text-2xl font-bold mb-6 text-center underline bg-clip-text text-transparent animate-title-gradient">
-          Product Gallery
-        </h1>
+    <h1 className="text-lg md:text-xl font-bold text-center underline bg-clip-text text-transparent animate-title-gradient">
+      Product Gallery
+    </h1>
 
-      </div>
+</div>
 
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {images.map((item, index) => {
