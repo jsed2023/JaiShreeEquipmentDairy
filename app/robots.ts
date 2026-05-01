6ea1b40fb55b3ef18207a968e50d2be2d6a9427a
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-05-01 19:28:53 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,13 +2,11 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/admin/", "/private/"],
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/admin/", "/private/"],
+    },
     sitemap: [
       "https://jaishreeequipmentdairy.in/sitemap.xml",
       "https://jaishreeequipmentdairy.in/location-sitemap.xml",
