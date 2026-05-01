e2a31749141831f6130df8b8e3442319edb0feec
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-05-01 19:33:01 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const url = request.nextUrl;
 
+  // remove trailing slash (except root)
   if (url.pathname !== "/" && url.pathname.endsWith("/")) {
     return NextResponse.redirect(
       new URL(url.pathname.slice(0, -1), request.url)
@@ -14,5 +15,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/:path*",
+  matcher: [
+    "/((?!robots.txt|sitemap.xml|location-sitemap.xml|image-sitemap.xml).*)",
+  ],
 };
