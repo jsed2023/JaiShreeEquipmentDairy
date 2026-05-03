1404dfe52d01c06ae5e5456ba8559ac0b0fa1457
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-05-03 15:03:15 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -10,7 +10,6 @@ export default function robots(): MetadataRoute.Robots {
     sitemap: [
       "https://jaishreeequipmentdairy.in/sitemap.xml",
       "https://jaishreeequipmentdairy.in/image-sitemap.xml",
-      "https://jaishreeequipmentdairy.in/location-sitemap.xml",
     ],
   };
 }
