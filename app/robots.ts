7945f412ce66e8ecb212f7ffdf6240e7f9425699
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-05-19 15:22:56 +0530

    Refactor robots function to use array for rules

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,18 @@
 import { MetadataRoute } from "next";
 import { siteConfig } from "@/config/site";
 
+export const dynamic = "force-static";
+
+const SITE_URL = siteConfig.url.replace(/\/$/, "");
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: `${siteConfig.url}/sitemap.xml`,
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
