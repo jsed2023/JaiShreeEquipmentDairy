0a3a6f84589c7720df0b2d7ca87d0ef21c15903d
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-06-17 23:24:04 +0530

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,23 @@
+import { MetadataRoute }
+from "next";
+
+import { siteConfig }
+from "@/config/site";
+
+export default function robots():
+MetadataRoute.Robots {
+
+  return {
+
+    rules: [
+      {
+        userAgent: "*",
+
+        allow: "/",
+      },
+    ],
+
+    sitemap:
+      `${siteConfig.url}/sitemap.xml`,
+  };
+}
