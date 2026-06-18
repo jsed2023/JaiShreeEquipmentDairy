766dc215397cbf36879b4af776bba8c6f6424019
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-06-19 03:41:53 +0530

    Update page.tsx

--- app/milk-testing-machine-spare-parts/page.tsx
+++ app/milk-testing-machine-spare-parts/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { spareParts } from "@/config/spareParts";
+import { spareparts } from "@/config/spareparts";
 
 export default function SparePartsPage() {
   return (
