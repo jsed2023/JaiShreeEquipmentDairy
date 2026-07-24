9223b209ddb0fa9542b7bb6494a3de46fe3c5a71
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-07-24 17:58:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,48 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false,
-  compress: true,
-  reactStrictMode: true,
-  poweredByHeader: false,
+trailingSlash: false,
+compress: true,
+reactStrictMode: true,
+poweredByHeader: false,
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/dddhtbuzs/**",
-      },
-    ],
-    formats: ["image/avif", "image/webp"],
-    minimumCacheTTL: 31536000,
-  },
+images: {
+remotePatterns: [
+{
+protocol: "https",
+hostname: "res.cloudinary.com",
+pathname: "/dddhtbuzs/**",
+},
+],
+formats: ["image/avif", "image/webp"],
+minimumCacheTTL: 31536000,
+},
 
-  experimental: {
-    scrollRestoration: true,
-  },
+experimental: {
+scrollRestoration: true,
+},
 
-  async rewrites() {
-    return [
-      {
-        source: "/milk-analyzer-:slug",
-        destination: "/locations/milk-analyzer-:slug",
-      },
-    ];
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/sitemap.xml",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/xml; charset=utf-8",
-          },
-        ],
-      },
-    ];
-  },
+async rewrites() {
+return [
+{
+source: "/milk-analyzer-:slug",
+destination: "/locations/milk-analyzer-:slug",
+},
+];
+},
 };
 
 module.exports = nextConfig;
