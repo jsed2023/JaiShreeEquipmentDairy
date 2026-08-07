f7c480d9325c50fb92cad56962d8ab147793e46e
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-08-07 20:50:36 +0530

    Update JSEDLoader.tsx

--- components/JSEDLoader.tsx
+++ components/JSEDLoader.tsx
@@ -1,19 +1,20 @@
 "use client";
-
+import Image from "next/image";
 export default function JSEDLoader() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-black z-9999">
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-white dark:bg-black">
+      <div className="relative flex items-center justify-center">
+        <div className="h-20 w-20 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600"></div>
 
-      <div className="loader">
-
-        <div className="ring"></div>
-
-        <span className="logo">
-          JSED
-        </span>
-
+        <Image
+          src="/logo.png"
+          alt="JSED Logo"
+          width={48}
+          height={48}
+          className="absolute"
+          priority
+        />
       </div>
-
     </div>
   );
 }
