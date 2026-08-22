8976136649e51eea1554218fbc7cbf372f0ff21d
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-08-22 14:10:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,12 +49,6 @@ export const metadata: Metadata = {
     icon: "/favicon.ico",
   },
 
-  /*
-   * Google Search Console verification
-   */
-  verification: {
-    google: "szRN11DRRCd9NtuijX2dAAtPfaV_EGAfuwSv_iM7t94",
-  },
 
   openGraph: {
     type: "website",
@@ -110,44 +104,12 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({
-                  'gtm.start': new Date().getTime(),
-                  event:'gtm.js'
-                });
-
-                var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),
-                    dl=l!='dataLayer'?'&l='+l:'';
-
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `,
-          }}
-        />
+        
+         
       </head>
 
       <body>
-        {/* IMPORTANT: GTM noscript must be the first body element */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{
-              display: "none",
-              visibility: "hidden",
-            }}
-          />
-        </noscript>
+        
 
         <LocalBusinessSchema />
 
