42865667308ac34ea8a9b92ad3259d61a903965f
Jai Shree Equipment Dairy <139153715+jsed2023@users.noreply.github.com>
2026-08-23 14:38:52 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,23 +100,31 @@ export default function RootLayout({
       data-scroll-behavior="smooth"
       suppressHydrationWarning
     >
-      <Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-RBFDTF7PWV"
-  strategy="afterInteractive"
-/>
+       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RBFDTF7PWV"
+          strategy="afterInteractive"
+        />
 
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RBFDTF7PWV');
+          `}
+        </Script>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P3FFNTJ9');</script>
 
-    gtag('config', 'G-RBFDTF7PWV');
-  `}
-</Script>
+      </head>
 
       <body>
-        
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P3FFNTJ9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
         <LocalBusinessSchema />
 
